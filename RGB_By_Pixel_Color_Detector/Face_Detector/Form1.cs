3e23dba33f52689ca67ab4b69c505e0cebc43a88
Tarek <tarek4u24@gmail.com>
2017-07-16 06:43:05 +0300

    Add files via upload

---
+++ RGB_By_Pixel_Color_Detector/Face_Detector/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using AForge.Imaging.Filters;
+
+namespace Face_Detector
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap _inputImage; //make a global variable to be accessable to all the loops
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void insert_button_Click(object sender, EventArgs e)
+        {
+            var openfileDialog = new OpenFileDialog();
+            if (openfileDialog.ShowDialog() == DialogResult.OK)
+            {
+                _inputImage = (Bitmap)System.Drawing.Image.FromFile(openfileDialog.FileName);
+                pictureBoxInput.Image = _inputImage;
+
+            }
+        }
+
+        private void process_button_Click(object sender, EventArgs e)
+        {
+            //changes are made here
+
+            //COLOUR PIXEL FILTERING
+            //if the image is withing the range else discard 
+            //point based application
+            var colourPixelFilter = new ColorFiltering();
+            colourPixelFilter.Red = new AForge.IntRange(90, 238);
+            colourPixelFilter.Green = new AForge.IntRange(60, 180);
+            colourPixelFilter.Blue = new AForge.IntRange(30, 200);
+            var colourPixelOutput = colourPixelFilter.Apply(_inputImage);
+            pictureBoxOutput.Image = colourPixelOutput;
+        }
+    }
+}
